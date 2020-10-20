3a1e4fe48afe0e35f887b8f9e9b59ac3b580a3df
Wesley Strange <wesley.ws.strange@gmail.com>
2020-10-20 11:44:05 -0700

    Hook App.js up to the Redux Store

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 import Homepage from './components/Homepage'
 import DeckDetails from './components/DeckDetails'
 import AddCard from './components/AddCard'
 import ErrorMessage from './components/ErrorMessage'
 import Quiz from './components/Quiz'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator()
+const store = createStore(reducer)
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Homepage} options={{headerShown:false}} />
-        <Stack.Screen name="DeckDetails" component={DeckDetails} />        
-        <Stack.Screen name="AddCard" component={AddCard} />
-        <Stack.Screen name="ErrorMessage" component={ErrorMessage} />
-        <Stack.Screen name="Quiz" component={Quiz} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Homepage} options={{headerShown:false}} />
+          <Stack.Screen name="DeckDetails" component={DeckDetails} />        
+          <Stack.Screen name="AddCard" component={AddCard} />
+          <Stack.Screen name="ErrorMessage" component={ErrorMessage} />
+          <Stack.Screen name="Quiz" component={Quiz} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>    
   )
 }
 
