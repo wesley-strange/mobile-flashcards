f4729c94b18c28cfd7e89edf7623746847a7f30a
Wesley Strange <wesley.ws.strange@gmail.com>
2020-10-21 09:19:20 -0700

    Update Quiz.js component

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
+
 import CardDetails from './CardDetails'
 
-export default function Quiz() {
-  return (
-    <View>
-      <Text>Quiz</Text>
-      <CardDetails />
-    </View>
-  )
+class Quiz extends Component {
+  state = {
+    correct: 0,
+    incorrect: 0,
+    numQuestions: this.props.deck.questions.length,
+    currentQuestion: 0,
+    showAnswer: false,
+  }
+  handleAnswer = (answer) => {
+    this.setState((prevState) => ({
+      [answer]: prevState[answer] + 1,
+      currentQuestion: prevState.currentQuestion + 1
+    }))
+  }
+  handleShowAnswer = () => {
+    this.setState((prevState) => ({
+      showAnswer: !prevState.showAnswer
+    }))
+  }
+  render () {
+    const { deck, navigation } = this.props
+    const { title, questions } = deck
+    const { correct, incorrect, numQuestions, currentQuestion, showAnswer } = this.state
+
+    if (questions.length === 0) {
+      return (
+        <ErrorMessage />
+      )
+    }
+
+    if (questionCount === currentQuestion) {
+      return (
+        <QuizResults 
+          numQuestions={numQuestions}
+          correct={correct}
+          incorrect={incorrect}
+          navigation={navigation}
+        />
+      )
+    }
+
+    return (
+      <View>
+        <CardDetails 
+          question={questions[currentQuestion]}
+          currentQuestion={currentQuestion}
+          questions={questions}
+          handleShowAnswer={this.handleShowAnswer}
+          handleAnswer={this.handleAnswer}
+          title={title}
+          showAnswer={showAnswer}
+        />
+      </View>
+    )
+  }  
 }
+
+function mapStateToProps ({ decks }, { route }) {
+  const { title } = route.params
+  const deck = decks[title]
+
+  return {
+    deck
+  }
+}
+
+export default connect(mapStateToProps)(Quiz)
